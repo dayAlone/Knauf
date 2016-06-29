5726e1de0c3c52d2c14bb235f55266a237a0958e
dayAlone <andrey.slider@gmail.com>
2016-06-29 18:32:40 +0700

    Changes

--- config/production.js
+++ config/production.js
@@ -4,5 +4,13 @@ export default {
     ...config,
     https: false,
     domain: '12tk.ru',
-    port: 80
+    port: 80,
+    db: {
+        dialect: 'postgres',
+        database: 'knauf',
+        host: 'node110218-teploknauf.jelastic.regruhosting.ru',
+        username: '',
+        password: '',
+        port: 5432
+    }
 }
