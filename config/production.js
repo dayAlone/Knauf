e82b9be413932ae28fa8366830b79dd71a50a0c7
dayAlone <andrey.slider@gmail.com>
2016-06-29 16:47:43 +0700

    Fix

--- config/production.js
+++ config/production.js
@@ -1,4 +1,8 @@
 import config from './default'
-config.domain = 'patriot.aviasales.ru'
-config.https = false
-export default config
+
+export default {
+    ...config,
+    https: false,
+    domain: '12tk.ru',
+    port: 80
+}
--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import initConnection from './libs/sequelize'
 
 const app = new Koa()
 
-import sendSms from './libs/sendSms'
+//import sendSms from './libs/sendSms'
 
 app.keys = [secret]
 
@@ -16,8 +16,8 @@ co(function*() {
     const connection = yield initConnection()
     const models = yield initModels(connection)
 
-    let result = yield sendSms('+79031826622', 'я повелитель смс')
-    console.log(result)
+    //let result = yield sendSms('+79031826622', 'я повелитель смс')
+    //console.log(result)
 
     initMiddlewares(app, connection)
     initControllers(app, models)
