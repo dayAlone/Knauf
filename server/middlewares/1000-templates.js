7917b09196e3f9a1f2de3ca8150d0ee3cf0b920f
dayAlone <andrey.slider@gmail.com>
2016-06-30 16:54:29 +0700

    Fix

--- server/middlewares/1000-templates.js
+++ server/middlewares/1000-templates.js
@@ -29,54 +29,7 @@ export default () => function* (next) {
 
         localsFull.user = this.req.user
 
-        localsFull.social = {
-            vkontakte: {
-                short: 'vk',
-                text: 'Вконтакте',
-                link: 'https://vk.com/aviasalesru',
-                utm: '?utm_source=share&utm_medium=vk_share&utm_content=share_button&utm_campaign='
-
-            },
-            facebook: {
-                short: 'fb',
-                text: 'Facebook',
-                link: 'https://www.facebook.com/aviasales.ru',
-                utm: '?utm_source=share&utm_medium=fb_share&utm_content=share_button&utm_campaign='
-
-            },
-            instagram: {
-                short: 'inst',
-                text: 'Instagram',
-                link: 'https://instagram.com/aviasales',
-            },
-            twitter: {
-                short: 'tw',
-                text: 'Twitter',
-                link: 'https://twitter.com/aviasales',
-                utm: '?utm_source=share&utm_medium=tw_share&utm_content=share_button&utm_campaign=',
-                shorts: {
-                    gmo: 'http://avs.io/fea',
-                    gaypride: 'http://avs.io/feb',
-                    terror: 'http://avs.io/fed',
-                    drugs: 'http://avs.io/fee',
-                    winner: 'http://avs.io/fef'
-                }
-            },
-            odnoklassniki: {
-                short: 'ok',
-                text: 'Одноклассники',
-                link: 'https://ok.ru/aviasales',
-                utm: '?utm_source=share&utm_medium=ok_share&utm_content=share_button&utm_campaign='
-            },
-            viber: {
-                short: 'vbr',
-                text: 'Viber',
-            },
-            google: {
-                short: 'gp',
-                text: 'Google+'
-            },
-        }
+        localsFull.cache = true
 
         const templatePathResolved = path.join(config.templates.root, `${templatePath}.pug`)
         return jade.renderFile(templatePathResolved, localsFull)
